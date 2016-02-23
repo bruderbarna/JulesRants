e773271604bafa07decd11e3e8f26aaae947fd94
Bruder Barna <bruder_barna@hotmail.com>
2016-02-23 17:45:18 +0100

    lots added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, :skip => [:registration, :password]
   root 'posts#index'
   resources :posts
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.new({:email => "julesrants@julesrants.com", :password => "111111", :password_confirmation => "111111" }).save(:validate => false)
